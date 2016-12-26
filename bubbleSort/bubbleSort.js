06de54d1d1ec0dd1036d692964ffd5079ac5138d
MoathDawahreh <moath.dawahreh@yahoo.com>
2016-12-26 15:04:14 +0200

    toyP

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -31,8 +31,25 @@
 // Feel free to add helper functions if needed
 
 var bubbleSort = function(array) {
+  var swapped;
+  do{
+    swapped=false
+    for (var i = 0; i < array.length-1; i++) {
+      if (array[i]>array[i+1]) {
+        var temp=array[i];
+        array[i]=array[i+1];
+        array[i+1]=temp
+        swapped=true
+
+      }
+          }
+
+  }while(swapped)
+  return array
   
 };
+
+
 
 /*
 

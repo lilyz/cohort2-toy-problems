df35eb6b23f28a3c4d04122e30905dae5f2af26b
saharAl-shurafat1990 <hayelsahar@gmail.com>
2016-12-31 11:44:23 +0300

    SaharToy1

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,3 +17,32 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
+	function rockPaperScissors(){
+		var x=["rock","paper","scissors"];
+		var result=[];
+
+		for (var i = 0; i < x.length; i++) {
+			for (var j = 0; j < x.length; j++) {
+				for (var k = 0; k < x.length; k++) {
+					 result.push([].concat(x[i],x[j],x[k]));
+				}
+			}
+		}
+		return JSON.stringify(result);
+		
+
+	}
+	function roPaSc(n){
+		var x=["rock","paper","scissors"];
+		var result=[];
+		if(n===0){
+			 result.push([].concat(rockPaperScissors()));
+			 return result;
+
+		}
+		else {
+			return roPaSc(n-1);
+		}
+
+
+	}

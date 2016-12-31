72075f7b679d22ced3924a7c63c357ea83daec4e
AbobakrAlduais <aalduais@gmail.com>
2016-12-31 10:49:07 +0200

    updat

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,3 +17,48 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
+
+
+/*
+* Write a function that generates every sequence of throws a single
+* player could throw over a three-round game of rock-paper-scissors.
+*
+* Your output should look something like:
+*   [["rock", "rock", "rock"],
+*    ["rock", "rock", "paper"],
+*    ["rock", "rock", "scissors"],
+*    ["rock", "paper", "rock"],
+             ...etc...
+     ]
+*
+* Extra credit:
+*   - Make your function return answers for any number of rounds.
+* Example:
+* rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
+*
+*/
+
+
+function rockPaperScissors(n){
+    var arr=[];
+    var result=[];
+    for (var i=1;i<num;i++){
+        for (var j=1;j<num;j++){
+            if (i===1){
+                arr.push("rock");
+                arr.push("rock")
+                arr.push("rock")
+                result.push(arr)
+            }
+            if(i===2 ){
+                arr.push("rock")
+                arr.push("rock")
+                arr.push("paper")
+                result.push(arr);
+            }
+            if (i===3);
+        }
+    }
+    }
+    return result;
+}

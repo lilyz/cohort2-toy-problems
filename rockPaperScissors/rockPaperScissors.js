94a1c8a449b603609d6ff4ac185de9193653f371
AmerAlOmari <ameroni1990@gmail.com>
2016-12-31 10:57:55 +0200

    to be continued

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,3 +17,30 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
+
+var f = [];
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+};
+
+function rockPaperScissors(n){
+	var arrayTotal, arraySmall, arrayOptions;
+	arrayOptions = ["rock", "paper", "scissors"];
+	arrayTotal = [];
+	if (n >= 3){
+	for (j = 1; j <= factorial(n); j++){
+		arraySmall = [];
+		for (i = 1; i <= n; i++){
+			arraySmall.push(arrayOptions[Math.floor(Math.random() * 3)]);
+		}
+			for (k = 0; k < arrayTotal.length; k++){}
+				if (arrayTotal[k]) // to be continued
+		arrayTotal.push(arraySmall);
+	}
+	return arrayTotal; 
+	}
+}

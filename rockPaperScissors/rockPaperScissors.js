d8b9b8c111ded5858de25637219bc6b4698ef3a3
DaniaHamdan <dandonzh@gmail.com>
2016-12-31 10:48:16 +0200

    toy

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -17,3 +17,37 @@
 * rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
 *
 */
+
+function generate(){
+	var result=[]
+	var player1="";
+	var player2="";
+	var player3="";
+	var round=["rock", "paper", "scissors"],
+  for (var i=0;i<9;i++){
+  	for(var j=0;j<3;j++){
+  	var arr=["rock"];
+  	arr.push(round[i])
+  	arr.push(round[j])
+  	result.push(arr);
+  }
+
+  	}
+for (var i=0;i<9;i++){
+	for(var j=0;j<3;j++){
+  	var arr=["paper"];
+  	arr.push(round[1])
+  	arr.push(round[i+1])
+  	result.push(arr);
+}
+  	}
+for (var i=0;i<9;i++){
+	for(var j=0;j<3;j++){
+  	var arr=["scissors"];
+  	arr.push(round[j])
+  	arr.push(round[i+1])
+  	result.push(arr);
+
+  	}
+  	return result
+}

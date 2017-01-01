2cc1a3097fb34f3c1f7899ecf2d4086c1e1b6d4c
AhmadAlMaradni <ahmad.maradni@hotmail.com>
2017-01-01 14:34:56 +0200

    anagram

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -14,10 +14,21 @@
   * var anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
+  var bigarr = [];
+  var str= "";
+function allAnagrams(r) {
 
-var allAnagrams = function(string) {
-	var array = [];
-	var newstr = "";
-	string = string.split(" ")
-
+  var i, char;
+  for (i = 0; i < r.length; i++) {
+    char = r.substr(i, 1)[0];
+    r=r.substr(i,r.length)
+    str+=char
+    if (r.length == 0) {
+      bigarr.push(str.slice());
+    }
+    allAnagrams(r);
+    r.substr(i, 0, char);
+    str= str.slice(-1)
+  }
+  return bigarr
 };

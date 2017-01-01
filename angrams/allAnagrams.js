d4157717116290fa5aa1dd847dc467fa806f1e4b
KamalHajjar <kamalhajjar907@gmail.com>
2017-01-01 10:26:50 +0200

    toy problem 2

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -16,6 +16,24 @@
   */
 
 var allAnagrams = function(string) {
-	
-
+var arr =[];
+var x = string.split("").reverse().join("");
+for(var s=0;s<2;s++){
+for (var k = 0; k < string.length; k++) {
+	var str =string[k]; 	
+	for (var i = 0; i < string.length; i++) {
+		if(str.indexOf(string[i])===-1){
+			str+=string[i]
+		}
+		for (var j = 0; j < string.length; j++) {
+			if(str.indexOf(string[j])===-1){
+			str+=string[j]
+			}
+		}
+	}
+arr.push(str)
+}
+string = string.split("").reverse().join("");
+}
+return arr;
 };

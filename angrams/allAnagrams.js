5d718cbca9ef484bf2e5fc736827e72a0fc07749
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-01 10:07:15 +0200

    Not solved

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -14,8 +14,37 @@
   * var anagrams = allAnagrams('abc');
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
+function shuffle(arr) {
+  var currI = arr.length, Value, randI;
 
+  while (0 !== currI) {
+
+    randI = Math.floor(Math.random() * currI);
+    currI -= 1;
+
+    Value = arr[currI];
+    arr[currI] = arr[randI];
+    arr[randI] = Value;
+  }
+
+  return arr;
+}
+
+
+var lastArr = []
 var allAnagrams = function(string) {
-	
+	if(lastArr.length === 0){
+		lastArr.push(string)
+	}
+	var arr = string.split("")
+	var x = shuffle(arr)
+	for (var i = 0; i < string.length; i++) {
+		if(lastArr.indexOf(x)>=0){
+			x=shuffle(arr)
+		}else{
+			allAnagrams(string)
+		}
+	}
+	return lastArr
 
 };

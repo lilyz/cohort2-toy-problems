da069efbb4dbbc0f9b2875686a243c6d25e8a1f5
cozmoc <cozmocozi@gmail.com>
2017-01-01 10:03:07 +0200

    up to date

---
+++ angrams/allAnagrams.js
@@ -0,0 +1,42 @@
+/**
+ * Given a single input string, write a function that outputs an array of strings with every possible
+ * combination of letters.
+ *
+ * At first, don't worry about repeated (duplicate) strings.
+ *
+ * What time complexity is your solution?
+ *
+ * Extra credit: De-duplicate your return array without using uniq().
+ */
+
+/**
+  * example usage:
+  * var anagrams = allAnagrams('abc');
+  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+  */
+
+var allAnagrams = function(string) {
+	var arr=string.split("");
+	var arr2=[];
+	var str="";
+	while(arr2.length!==arr.length*2){
+		for (var i=0 ; i<arr.length;i++){
+			var rand = arr[Math.floor(Math.random() * arr.length)];
+			str+=rand;
+		}
+		var strarr=str.split("");
+		for (var y = 0; y < strarr.length; y++) {
+			for (var x=0; x < strarr.length; x++){
+				if (strarr[x]===strarr[y] && y!==x){
+					str="";
+				}
+			}
+		}
+
+		if (!arr2.includes(str) && str!==""){
+			arr2.push(str);
+		}
+		str="";
+	}
+	return arr2;
+};

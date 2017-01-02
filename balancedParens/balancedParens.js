2f4797d8300ed6599a08afc834f56fca653de8c2
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-02 10:17:21 +0200

    toy_problem

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,7 +24,53 @@
  *	"())"
  */
 
- var balancedParens = function (input) {
-	
+
+ function compare(open,close) {
+ 	var result = true
+ 	if(open.length !== close.length){
+ 		return false
+ 	}else{
+ 		for (var i = 0; i < open.length; i++) {
+ 			if(open[i] === "(" && close[i] === ")"){
+ 				result = true
+ 			}else if (open[i] === "{" && close[i] === "}")
+		}
+ 	}  
+}
+ 	var balancedParens = function (input) {
+ 	// var result = true
+ 	// var arr = [] 
+ 	// var parens = {
+ 	// 	"{": "}",
+ 	// 	"[": "]",
+ 	// 	"(": ")"
+ 	// }
+ 	// var close = {
+ 	// 	"}":1,
+ 	// 	"]":1,
+ 	// 	"}":1
+ 	// }
+ 	// for (var i = 0; i < input.length; i++) {
+ 	// 	for(var key in close){
+ 	// 		var temp = input[i]
+ 	// 		if(parens[temp]){
+ 	// 			arr.push(parens[temp])
+ 	// 		}else if(temp === key){
+
+ 	// 		}
+ 	// 	}
+ 	// }
+ 	var close = []
+ 	var open = []
+ 	for (var i = 0; i < input.length; i++) {
+ 		if(input[i] === "(" ||input[i] === "{"||input[i] === "{" ){
+ 			open.push(input[i])
+ 		}else if (input[i] === "}"||input[i] === ")"||input[i] === "]"){
+ 			close.push(input[i])
+ 		}else{
+ 			continue
+ 		}
+ 	}
+ 	return compare(open,close)
+
  };
-	

ef29662a52023adb907af45ae205057ddf55ad8f
Zainab Hammami <zainab.hammami.zh@gmail.com>
2017-01-02 10:21:52 +0200

    mod3

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -25,6 +25,28 @@
  */
 
  var balancedParens = function (input) {
-	
- };
+ 	var x =input.length/2;
+	var y=input.length-x;
+	if (input === "(" || input === ")" || input ===")(" || input === "{" || input === "}" || input === "}{" || input === "[" || input === "]" || input === "][")  {
+		return false;
+	}
+	for (var i = 0 ; i<input.length; i++) {
+		if (input.length%2!==0) {
+			return false;
+		}
+		for(var j = 0; j<x ; j++) {
+			if (input[x]=="("&& input[y]==")" || input[x] === "["&& input[y] === "]" || input[x] === "{" && input[y]=== "}"){
+			}
+				return true;
+		}
+		for (var t = 0; t<x ; t++) {
+			for (var s = 0; s<x; s++) {
+				if (input[x] === "(" && input[y] === ")" || input[x] === "{" && input[y] === "}" || input[x] === "[" && input[y] === "]") {
+				}
+			}
+		return true;
+		}
+	}
+	return false;
+};
 	

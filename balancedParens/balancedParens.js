fe5d4eab7ac2f21a2292d18ab4450c8fb5b59388
Ola-Alkhateeb <ola.93.alkhateeb@gmail.com>
2017-01-02 09:56:28 +0200

    toy day 3

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -54,3 +54,29 @@
  	return open1 === closed1 && open2 === closed2 && open3 === closed3 
 
  };
+
+// function balancedParens(string){  
+//   var stack = [];
+//   var pairs = {
+//     '[':']',
+//     '{':'}',
+//     '(':')'
+//   };
+//   var closers = {
+//     ')': 1,
+//     ']': 1,
+//     '}': 1
+//   };
+//   for(var i = 0; i < string.length; i++){
+//     if(pairs[string[i]]){
+//       stack.push(pairs[string[i]]);
+//     } else if(string[i] in closers){
+//       if(stack[stack.length -1] === string[i]){
+//         stack.pop();
+//       } else{
+//         return false;
+//       }
+//     }
+//   }
+//   return !stack.length;
+// }

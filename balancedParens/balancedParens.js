e44c32c51736d5e5dcd58811eea0aba170f62df7
KamalHajjar <kamalhajjar907@gmail.com>
2017-01-02 10:03:44 +0200

    toy problem 3

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -25,6 +25,21 @@
  */
 
  var balancedParens = function (input) {
-	
- };
+	var x;
+	var str=""
+	for (var i = 0; i <input.length; i++) {
+		x=input[i]
+		if (x==="["||x==="]"||x==="{"||x==="}"||x==="("||x===")"||x==="'"||x==='"'){
+			str+=x;
+		}
+	}
+	var j=str.length;
+	for (var i = 0; i < str.length/2; i++){
+		x=str[i].charCodeAt() + str[j-1].charCodeAt()
+		if(x!==81&&x!==248&&x!==68&&x!==78&&x!==184)
+			return false;
+	j--;
+	}
+	return true;
+};
 	

bf16b06b391b0ad599becc2a80d81d091880f6ad
DaniaHamdan <dandonzh@gmail.com>
2017-01-03 10:39:50 +0200

    toy 4

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -11,7 +11,36 @@
  */
 
 //Works for any number of input strings:
-var commonCharacters = function(string1, string2) {
+var commonCharacters = function(string1, string2) {;
+	var result="";
+	for(var i=0;i<string1.length;i++){
+		if(string1.includes(string1[i])&&string2.includes(string1[i])){
+		if(!result.includes(string1[i])){
+			result+=string1[i]
+		}
+		}
+	}
+	result = result.replace(/\s/g, '');
+	return result
   
 };
+var commonCharacters2=function(string){
+	var str=""
+	var com=true
+	var array=Array.from(arguments)
+	var x=array.length
+	for(var j=0;j<string.length;j++){
+	for(var i=0;i<x;i++){
+		com=(array[j].includes(string[i])&&com)
+		console.log(array[j],string[i])		
+	}
+	if((!str.includes(string[i]))&&com){
+		str+=string[i]
+		}
+}
+		str = str.replace(/\s/g, '');
+			return str
+	}
+
+
 

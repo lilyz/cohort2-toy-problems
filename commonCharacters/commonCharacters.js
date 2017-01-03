1ba8c4db3a0b6630967b331b52c97782ed033d0a
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-03 12:27:10 +0400

    solution

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -12,6 +12,30 @@
 
 //Works for any number of input strings:
 var commonCharacters = function(string1, string2) {
-  
+	var res = [];
+	for (var i = 0; i < string1.length; i++) {
+	  	if(string2.indexOf(string1[i]) > -1 ){
+	  		res.push(string1[i]);
+	  	}
+	}
+	return res.join("");  
 };
 
+
+
+//try for extra credit 
+var commonCharacters = function(string1, string2) {
+	
+	var arg = Array.from(arguments);
+	
+	var res = "";
+	if(i>=arg.length){
+		return res;
+	}
+	for (var i = 0; i < arg[0].length; i++) {
+	  	if(arg[1].indexOf(arg[0][i]) > -1 ){
+	  		return res + arg[0][i] + commonCharacters(arg[0],arg[i+1]);	
+	  	}
+	}
+	
+};

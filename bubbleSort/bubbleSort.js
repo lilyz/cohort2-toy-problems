d4f933572b5ce169c282bb8fb8e1f1ff9a02f287
abdulazeeznaji <abdulazeeznaji@yahoo.com>
2017-01-04 09:47:17 +0200

    new

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -31,7 +31,17 @@
 // Feel free to add helper functions if needed
 
 var bubbleSort = function(array) {
-  
+	var x;	
+	for(var i= 0; i< array.length-1; i++){
+		for(var j= 0; j< array.length-1; j++){
+			if(array[j] > array[j+1]){
+				x=array[j]
+				array[j]=array[j+1]
+				array[j+1]=x
+			} 
+		}
+	}
+	return array;
 };
 
 /*

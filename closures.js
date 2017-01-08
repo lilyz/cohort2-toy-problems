6709e035de9e1b8ec2f68ebae857acac94abb92e
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-08 11:55:46 +0400

    closures

--- closures.js
+++ closures.js
@@ -14,6 +14,29 @@ square(3);//should return 9
 cube(3);//should return 27
 */
 
+
+
+var pow = function (exp){
+
+	var res = 1;
+	return function(num){
+		if(exp === 0 ){
+			return 1;
+		}
+		while(exp!==0){
+			res*=num;
+			exp--;
+		}
+		return res;	
+	}
+}
+
+
+
+
+
+
+
 /*
 Problem 2:
 Write a function pingPongTracker that accepts no arguments and returns an object with the
@@ -24,7 +47,7 @@ following methods:
 - myLevel() should return your experience level based on how much time you have spent playing
   pingpong. These are the levels: a) Less than 30 minutes - "I need to improve my game"
                                   b) 30-100 minutes - "You need to improve your game"
-                                  c) More than 100 minutes - "Wow, I have wasted a lot of time"
+                                  c) More than 100 minutes - "You need to improve your game"
 
 Example:
 var myGame = pingPongTracker();
@@ -33,3 +56,38 @@ myGame.playOneGame();//should return "Game played";
 myGame.timeSpentPlaying(); //should return 30;
 myGame.myLevel(); //should return "You need to improve your game"
 */
+
+
+
+
+function pingPongTracker(){
+	var obj = {};
+
+	obj.time = 0;
+	obj.timeSpentPlaying = timeSpentPlaying;
+	obj.playOneGame = playOneGame;
+	obj.myLevel = myLevel;
+
+	return obj;
+}
+
+var timeSpentPlaying = function(){
+	return this.time;
+}
+
+var playOneGame = function(){
+	this.time = this.time + 15;
+	return "Game Played";
+}
+
+var myLevel = function(){
+	if(this.time < 30){
+		return "I need to improve my game";
+	}
+	else if(this.time < 101){
+		return "You need to improve your game";
+	}
+	else{
+		return "You need to improve your game";
+	}
+}

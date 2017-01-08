6f83ef882f63dda7dcbff721b9fad3595c187c3e
hamzaahmad <zeroday60@gmail.com>
2017-01-08 09:49:28 +0400

    Clousers

--- closures/closures.js
+++ closures/closures.js
@@ -13,3 +13,39 @@ var cube = pow(3);
 square(3);//should return 9
 cube(3);//should return 27
 */
+
+var pow=function (expo) {
+ ex=expo
+ return{
+    pow:function(num){
+   return Math.pow(num,ex)
+   }
+ 
+}
+}
+
+
+var pingPongTracker =function(){
+	var counter=0 /// i make it like minute
+	setInterval(function(){counter++; }, 100);
+	return{
+		timeSpentPlaying:function(){
+          return counter
+		}
+		playOneGame:function(){
+			counter=counter+15
+			return "Game Played" + counter
+		}
+		myLevel:function(){
+			if(counter < 30 ){
+				return "I need to improve my game"
+			}
+			if(counter>30 && counter < 100){
+				return "You need to improve your game"
+			}
+			if(counter > 100){
+	 		return "Wow, I have wasted a lot of time"
+			}
+		}
+	}
+}

5808b457aa23a1ab2487f2e1ee136b0008386bf5
abdulazeeznaji <abdulazeeznaji@yahoo.com>
2017-01-09 10:56:57 +0300

    not solved

--- coinSum/coinSum.js
+++ coinSum/coinSum.js
@@ -12,3 +12,24 @@ Given that total amount of pences, calculate the number of ways to create that a
 Example:
 coinCombination(200p) //-> 73682
 */
+
+// what i understand from the quseion is that he will gave me 
+// the money in pence and i will have to cheak how much i can git out from it 
+// pound and pence i have to use currency  of 1 $ and and 2 and give the number if how many ways 
+//that i can creat out of this amount 
+
+
+function currency(input){
+	var arr=[1,2,5,10,20,50,100,200];
+	var counter=0;
+	//first i stored all the change of the  coians
+	//now if he gave me 100 i have to start giviing him from 1 50p , 2 20p ,5 p,3 p , 3 1p
+	
+	for(var a=0; a < arr.length; a++ ){
+		for(var b = 0; b<arr.length; b++){
+			if(arr[a]+arr[b]){
+		}
+	}
+
+
+}

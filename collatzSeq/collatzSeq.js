06c2dc79698d596a6a1c80a7c1c91157ed188212
Mohannad Bakkar <mohannadbakkar@gmail.com>
2017-01-10 09:18:43 +0200

    returning the number of chain

--- collatzSeq/collatzSeq.js
+++ collatzSeq/collatzSeq.js
@@ -7,7 +7,8 @@ n → 3n + 1 (n is odd)
 Using the rule above and starting with 13, we generate the following sequence:
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 
-It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), 
+it is thought that all starting numbers finish at 1.
 
 Which starting number, under one million, produces the longest chain?
 
@@ -34,10 +35,12 @@ var result = [num]
 }
 function looping (){
   var max = 0
+  var num = 0
   for (var i = 0; i < 1000000; i++) {
     if(longestCollatzSeq(i) > max){
       max = longestCollatzSeq(i)
+      num =i
     }
   }
-  return max;
+  return num;
 }
